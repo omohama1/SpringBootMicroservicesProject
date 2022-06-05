140f465a96680e07bb56f1513023364ded4d5de4
Omar Qasirov <omarbmohamad@gmail.com>
2022-06-05 10:33:34 -0500

    Adding account microservice

---
+++ PhotoAppApiAccountManagement/src/test/java/com/om/photoapp/api/accountmanagement/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.om.photoapp.api.accountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
