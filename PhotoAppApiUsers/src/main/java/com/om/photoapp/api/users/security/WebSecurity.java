24b5bdd58cddc7da01c15657fba485172f4dfb5b
Omar Qasirov <omarbmohamad@gmail.com>
2022-06-11 16:25:07 -0500

    encrypting passwords

--- PhotoAppApiUsers/src/main/java/com/om/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/om/photoapp/api/users/security/WebSecurity.java
@@ -12,6 +12,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
+		http.headers().frameOptions().disable();
 		http.authorizeRequests().antMatchers("/users/**").permitAll();
 	}
 
